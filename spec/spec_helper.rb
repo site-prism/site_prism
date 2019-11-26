90604d0e3f2806110b418d6680c9fa8f9e24ee71
Luke Hill <lukehill_uk@hotmail.com>
2019-11-26 18:03:27 +0100

    Revert back to old config for dual management

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,13 +2,13 @@
 
 require 'simplecov'
 
-SimpleCov.start do
-  add_group 'Features', 'features'
-  add_group 'Specs', 'spec'
-  add_group 'Code', 'lib'
-end
-
-SimpleCov.minimum_coverage 99
+# SimpleCov.start do
+#   add_group 'Features', 'features'
+#   add_group 'Specs', 'spec'
+#   add_group 'Code', 'lib'
+# end
+#
+# SimpleCov.minimum_coverage 99
 
 require 'capybara'
 require 'capybara/dsl'
