8f8b552f1aec4a2f7680e92ac98d013a8ba262be
Luke Hill <lukehill_uk@hotmail.com>
2020-07-15 15:29:13 +0100

    Fixes to get all specs to pass

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,7 +22,7 @@ module SitePrism
     module_function
 
     def present_on_page
-      %i[element_one elements_one section_one sections_one element_three no_way_should_this_be_written]
+      %i[element_one elements_one section_one sections_one element_three]
     end
 
     def present_on_section
