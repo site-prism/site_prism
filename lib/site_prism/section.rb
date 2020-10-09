fa89ce22bb3d90ce5cb1dd22d135a09ed22029d9
Ivan Nievierov <i.nievierov@fetchrewards.com>
2020-10-09 12:10:41 -0500

    Use delegators instead of define method

--- lib/site_prism/section.rb
+++ lib/site_prism/section.rb
@@ -28,20 +28,17 @@ module SitePrism
       within(&block) if block_given?
     end
 
-    ::Capybara::Session::NODE_METHODS.each do |method|
-      class_eval <<~METHOD, __FILE__, __LINE__ + 1
-        def #{method}(...)
-          @root_element.method("#{method}").call(...)
-        end
-      METHOD
+    ROOT_ELEMENT_METHODS = ::Capybara::Session::NODE_METHODS + [:native]
+    SESSION_METHODS = ::Capybara::Session::DSL_METHODS - ::Capybara::Session::NODE_METHODS
+
+    private_constant :ROOT_ELEMENT_METHODS, :SESSION_METHODS
+
+    ROOT_ELEMENT_METHODS.each do |method|
+      def_delegators :root_element, method
     end
 
-    (::Capybara::Session::DSL_METHODS - ::Capybara::Session::NODE_METHODS).each do |method|
-      class_eval <<~METHOD, __FILE__, __LINE__ + 1
-        def #{method}(...)
-          @parent.method("#{method}").call(...)
-        end
-      METHOD
+    SESSION_METHODS.each do |method|
+      def_delegators :capybara_session, method
     end
 
     def to_capybara_node
@@ -54,20 +51,16 @@ module SitePrism
 
     # This is no longer a necessary method should probably be deprecated/removed
     def page
+      SitePrism::Deprecator.deprecate('Using page inside section')
       return root_element if root_element
 
       SitePrism.logger.warn('Root Element not found; Falling back to Capybara.current_session')
-      Capybara.current_session
+      capybara_session
     end
 
     def visible?
       @root_element.visible?
     end
-
-    def_delegators :capybara_session,
-                   :execute_script,
-                   :evaluate_script,
-                   :within_frame
 
     def capybara_session
       Capybara.current_session
@@ -77,10 +70,6 @@ module SitePrism
       candidate = parent
       candidate = candidate.parent until candidate.is_a?(SitePrism::Page)
       candidate
-    end
-
-    def native
-      root_element.native
     end
   end
 end
