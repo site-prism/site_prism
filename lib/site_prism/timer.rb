979e38206425923a51234ae1afd92dacbb1a07a6
Luke Hill <lukehill_uk@hotmail.com>
2021-03-16 16:34:13 +0000

    Add a little bit of docs

--- lib/site_prism/timer.rb
+++ lib/site_prism/timer.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 module SitePrism
+  # [SitePrism::Timer]
+  #
+  # Used to count asynchronously towards an overall desired duration or condition (Block)
   class Timer
     attr_reader :wait_time
 
+    # Return &block
+    #
+    # Count towards a specified time (Supplied)
     def self.run(wait_time, &block)
       new(wait_time).run(&block)
     end
@@ -13,10 +19,16 @@ module SitePrism
       @done = false
     end
 
+    # Return Boolean
+    #
+    # Whether the timer has completed
     def done?
       @done == true
     end
 
+    # Return &block
+    #
+    # Start the Timer and re-evaluate the block repeatedly
     def run
       start
       yield self
@@ -24,6 +36,9 @@ module SitePrism
       stop
     end
 
+    # Return [Boolean, Nil]
+    #
+    # Start the Timer in a separate process
     def start
       stop
       return if wait_time.zero?
@@ -35,6 +50,9 @@ module SitePrism
       end
     end
 
+    # Return True
+    #
+    # Forcibly stop the timer, and kill any threads created by it
     def stop
       if @thread
         @thread.kill
