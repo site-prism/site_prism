e2c4728f4feb468edec730e0acb8835139be9b86
Teppei Yamaguchi <teyamagu@gmail.com>
2021-11-26 21:57:43 +0900

    add swallow_timeout

--- spec/site_prism/waiter_spec.rb
+++ spec/site_prism/waiter_spec.rb
@@ -29,23 +29,27 @@ describe SitePrism::Waiter do
       let(:sleep_duration_long) { 0.5 }
       let(:sleep_duration_short) { 0.01 }
 
-      # rubocop:disable RSpec/MultipleExpectations
-      # rubocop:disable Style/Semicolon
       it 'when setting sleep_duration > timeout, error raise and yield execute 2 times' do
         count = 0
-        expect { described_class.wait_until_true(timeout, sleep_duration_long) { count += 1; false } }
-          .to raise_error(SitePrism::TimeoutError)
+        swallow_timeout do
+          described_class.wait_until_true(timeout, sleep_duration_long) do
+            count += 1
+            false
+          end
+        end
         expect(count).to eq(2)
       end
 
       it 'when setting sleep_duration < timeout, error raise and yield execute many times' do
         count = 0
-        expect { described_class.wait_until_true(timeout, sleep_duration_short) { count += 1; false } }
-          .to raise_error(SitePrism::TimeoutError)
+        swallow_timeout do
+          described_class.wait_until_true(timeout, sleep_duration_short) do
+            count += 1
+            false
+          end
+        end
         expect(count).to be >= 10
       end
-      # rubocop:enable Style/Semicolon
-      # rubocop:enable RSpec/MultipleExpectations
     end
   end
 end
