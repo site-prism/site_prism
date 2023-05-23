a9d4925746a33d915a21306a9f5a981c3c74bf7b
Luke Hill <lukehill_uk@hotmail.com>
2023-05-23 18:14:28 +0100

    Introduce copied app into spec support

--- spec/support/app.rb
+++ spec/support/app.rb
@@ -4,7 +4,19 @@ module SitePrism
   module Support
     class App
       def call(_env)
-        [200, { 'Content-Length' => '9' }, ['MyTestApp']]
+        [200, {}, [html]]
+      end
+      
+      def html
+        <<~HTML.gsub(/^\s+/, '')
+          <html>
+            <head></head>
+            <body>
+              <a href="#" class="foo">foo-link</a>
+              <table id="my-table">a table here</table>
+            </body>
+          </html>
+        HTML
       end
     end
   end
