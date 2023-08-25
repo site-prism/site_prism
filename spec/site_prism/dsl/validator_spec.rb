0e565636b22dd2fb48f7c325eb7102f3c0c200df
Luke Hill <lukehill_uk@hotmail.com>
2023-08-25 11:48:45 +0100

    Fix spec name

--- spec/site_prism/dsl/validator_spec.rb
+++ spec/site_prism/dsl/validator_spec.rb
@@ -7,39 +7,39 @@ describe SitePrism::DSL::Validator do
     end
   end
 
-  describe '.invalid?' do
+  describe '.name_invalid?' do
     context 'with a blacklisted prefix' do
-      subject { validator.invalid?('no_we_are_not_allowed') }
+      subject { validator.name_invalid?('no_we_are_not_allowed') }
 
       it { is_expected.to be true }
     end
 
     context 'with a blacklisted suffix' do
-      subject { validator.invalid?('i_am_also_not_allowed_') }
+      subject { validator.name_invalid?('i_am_also_not_allowed_') }
 
       it { is_expected.to be true }
     end
 
     context 'with invalid characters' do
-      subject { validator.invalid?('this_contains%20!invalidCH4RZ$') }
+      subject { validator.name_invalid?('this_contains%20!invalidCH4RZ$') }
 
       it { is_expected.to be true }
     end
 
     context 'with a starting upper-case character' do
-      subject { validator.invalid?('MUST_start_lowercase') }
+      subject { validator.name_invalid?('MUST_start_lowercase') }
 
       it { is_expected.to be true }
     end
 
     context 'with an invalid name' do
-      subject { validator.invalid?('attributes') }
+      subject { validator.name_invalid?('attributes') }
 
       it { is_expected.to be true }
     end
 
     context 'with a valid string' do
-      subject { validator.invalid?('abcdef123_XYZ') }
+      subject { validator.name_invalid?('abcdef123_XYZ') }
 
       it { is_expected.to be_nil }
     end
