175d18635c72250a82654680ec116dfa49a16b62
souchan2000 <ayasou1113@gmail.com>
2023-09-15 08:42:31 +0900

    delete comment

--- features/support/pages/shadow_root.rb
+++ features/support/pages/shadow_root.rb
@@ -4,7 +4,6 @@ class ShadowRoot < SitePrism::Page
   set_url '/shadow_root.htm'
   set_url_matcher(/shadow_root\.htm$/)
 
-  # shadow-root
   section :shadow_root_section, '#shadow-root', shadow_root: true do
     element :some_text, 'p'
   end
