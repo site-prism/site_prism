f32776653cc92691153dc6b4da5f0eb49645b693
Luke Hill <lukehill_uk@hotmail.com>
2024-06-12 10:37:09 +0100

    Add tests for not permitting blocks in element/elements

--- spec/site_prism/elements_spec.rb
+++ spec/site_prism/elements_spec.rb
@@ -1,25 +1,35 @@
 # frozen_string_literal: true
 
 describe 'Elements' do
-  describe '.elements' do
-    it 'can be set on `SitePrism::Page`' do
-      expect(SitePrism::Page).to respond_to(:elements)
-    end
-
-    it 'can be set on `SitePrism::Section`' do
-      expect(SitePrism::Section).to respond_to(:elements)
-    end
-  end
-
   shared_examples 'a page' do
+    describe '.elements' do
+      it 'can be set on `SitePrism::Page`' do
+        expect(SitePrism::Page).to respond_to(:elements)
+      end
+
+      it 'can be set on `SitePrism::Section`' do
+        expect(SitePrism::Section).to respond_to(:elements)
+      end
+
+      it 'cannot be defined with a build time block' do
+        expect { invalid_page }.to raise_error(SitePrism::UnsupportedBlockError)
+      end
+    end
+
     it 'returns an enumerable `Capybara::Result`' do
-      expect(page.elements_one).to be_a Capybara::Result
+      expect(page.elements_one).to be_a(Capybara::Result)
     end
   end
 
   context 'with a Page defined using CSS locators' do
     let(:page) { CSSPage.new }
     let(:klass) { CSSPage }
+    let(:invalid_page) do
+      Class.new(CSSPage) do
+        elements :fail, 'span.not-found' do
+        end
+      end
+    end
 
     it_behaves_like 'a page'
   end
@@ -27,6 +37,12 @@ describe 'Elements' do
   context 'with a Page defined using XPath locators' do
     let(:page) { XPathPage.new }
     let(:klass) { XPathPage }
+    let(:invalid_page) do
+      Class.new(XPathPage) do
+        elements :fail, '//span[@class="not-found"]' do
+        end
+      end
+    end
 
     it_behaves_like 'a page'
   end
